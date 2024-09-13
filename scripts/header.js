5839a38b6aedb01f49c501ed114be16c19edc6a1
Brandeau <diego.andrade.y@gmail.com>
2024-09-13 16:25:25 -0300

    chore: add documentation

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,9 @@
+/**
+ * Custom class for a header
+ */
 class CustomHeader extends HTMLElement {
 
-    //for desktops
+    //Media query for the window size of the device
     query = window.matchMedia('(min-width: 601px)');
 
     constructor(){
@@ -10,6 +13,8 @@ class CustomHeader extends HTMLElement {
         this.style.display = 'flex';
         this.style.height = '80px'
         this.style.padding = '10px';
+
+        //Custom styles for different screen sizes
         if(this.query.matches){
             this.style.flexDirection = 'row';
             this.style.justifyContent = 'space-between';
@@ -23,8 +28,9 @@ class CustomHeader extends HTMLElement {
         const logo = document.createElement('img');
         logo.src = '../media/logo-white.png';
 
+        //Custom styles for different screen sizes
         if(this.query.matches){
-            //completar
+            //fill out later
   
         }else{
             logo.style.width = '5em';
@@ -39,6 +45,7 @@ class CustomHeader extends HTMLElement {
         phoneNumber.style.fontFamily = 'Arial Black';
         phoneNumber.style.color = 'white';
 
+        //Custom styles for different screen sizes
         if(this.query.matches){
             phoneNumber.style.fontSize = '25px'
         }
