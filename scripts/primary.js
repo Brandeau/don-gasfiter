c815a6b084fbaed4e8ba0d8cda7aad710c121978
Brandeau <diego.andrade.y@gmail.com>
2024-10-03 12:57:18 -0300

    refactor: rewrite around functions and add handler

--- scripts/primary.js
+++ scripts/primary.js
@@ -1,35 +1,39 @@
 import {fetchFile} from "./helpers/fetch.js";
 
-/*Partials section */
-const imagePartial = await fetchFile("./templates/partials/image.hbs", "text");
-const paragraphPartial = await fetchFile("./templates//partials/paragraph.hbs", "text");
-const listPartial = await fetchFile("./templates/partials/list.hbs", "text");
-const videoPartial = await fetchFile("./templates/partials/video.hbs", "text");
-const buttonPartial = await fetchFile("./templates/partials/button.hbs", "text");
+async function registerPartials(){
 
+    Handlebars.registerPartial(
+        {
+            imagePartial: await fetchFile("./templates/partials/image.hbs", "text"),
+            paragraphPartial: await fetchFile("./templates//partials/paragraph.hbs", "text"),
+            listPartial: await fetchFile("./templates/partials/list.hbs", "text"),
+            videoPartial: await fetchFile("./templates/partials/video.hbs", "text"),
+            buttonPartial: await fetchFile("./templates/partials/button.hbs", "text")
+        }
+    )
+}
 
-Handlebars.registerPartial(
-    {
-        imageTemplate: imagePartial,
-        paragraphTemplate: paragraphPartial,
-        listTemplate: listPartial,
-        videoTemplate: videoPartial,
-        buttonTemplate: buttonPartial
-    }
-)
+async function renderNavbar(data){
+    const linkList = await fetchFile("../templates/linkList.hbs", "text")
+    const compiledlinkList = Handlebars.compile(linkList);
+    const navbar = document.querySelector("nav");
+    navbar.innerHTML = compiledlinkList(data.links);
+}
 
-/*Business logic*/
+async function renderSections(data){
 
-const data = await fetchFile("../data/content.json");
-const linkList = await fetchFile("../templates/linkList.hbs", "text")
-const compiledlinkList = Handlebars.compile(linkList);
-const navbar = document.querySelector("nav");
-navbar.innerHTML = compiledlinkList(data);
-const main = document.querySelector('main');
-const section = await fetchFile("../templates/section.hbs", "text");
-const compiledSection = Handlebars.compile(section);
+    const handle = await handleListItemClick.bind(null, data);
 
-function handleListItemClick(){
+    const navLinks = document.querySelector("nav ul")
+    navLinks.addEventListener("click", handle);   
+    
+}
+
+async function handleListItemClick(data, event){
+    const section = await fetchFile("../templates/section.hbs", "text");
+    const compiledSection = Handlebars.compile(section);
+    const main = document.querySelector('main');
+    
     if(event.target.id === 'sarro'){
         main.innerHTML = compiledSection(data.sarro)
     }else if(event.target.id === 'alcantarillado'){
@@ -41,9 +45,24 @@ function handleListItemClick(){
     }else if(event.target.id === 'griferias'){
         main.innerHTML = compiledSection(data.griferias)
     }
+} 
+
+async function renderHome(data){
+    
+    const section = await fetchFile("../templates/section.hbs", "text");
+    const compiledSection = Handlebars.compile(section);
+    const main = document.querySelector('main');
+    main.innerHTML = compiledSection(data.home)
+
 }
 
-const navLinks = document.querySelector("nav ul")
-navLinks.addEventListener("click", handleListItemClick);
+async function renderApp(){
+    const data = await fetchFile("../data/content.json");
 
-main.innerHTML = compiledSection(data.home)
+    await registerPartials();
+    await renderNavbar(data);
+    await renderHome(data);
+    await renderSections(data)
+}
+
+renderApp();
